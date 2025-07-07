f7d647b9bc40306e34b263d14eb0f0ca836451c3
AshishBhardwaj1 <fireash70@gmail.com>
2025-07-07 11:28:03 +0530

    frontend and backend connected

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,29 +1,31 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css";
-// import { BrowserRouter } from "react-router-dom";
-// import { AuthContextProvider } from "./context/AuthContext.jsx";
-// import { SocketContextProvider } from "./context/SocketContext.jsx";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-// 	<React.StrictMode>
-// 		<BrowserRouter>
-// 			<AuthContextProvider>
-// 				<SocketContextProvider>
-// 					<App />
-// 				</SocketContextProvider>
-// 			</AuthContextProvider>
-// 		</BrowserRouter>
-// 	</React.StrictMode>
-// );
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // Assuming App.jsx is your main component
-import './index.css'; // Optional: for global styles
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<AuthContextProvider>
+				<App />
+			</AuthContextProvider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App.jsx'; // Assuming App.jsx is your main component
+// import './index.css'; // Optional: for global styles
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <BrowserRouter>
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   </BrowserRouter>
+// );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,20 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
      tailwindcss(),
-  ],
+
+    ],
+     server: {
+       port: 3000,
+       proxy: {
+         "/api": {
+           target: "http://localhost:5000",
+         },
+       },
+     },
 })
+
+
+
+
+
+
