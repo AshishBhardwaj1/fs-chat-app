f80276e570a2dfc6c4cfc06ac2e6889d20e5c468
AshishBhardwaj1 <fireash70@gmail.com>
2025-07-07 11:52:35 +0530

    socket part

--- backend/server.js
+++ backend/server.js
@@ -7,21 +7,21 @@ import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDb from "./db/connectToMongoDb.js";
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
-const app = express()
+import { app, server } from "./socket/socket.js";
 
 app.use(express.json())
 app.use(cookieParser())
 
+const  port = process.env.PORT|| 5000;
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users",userRoutes)
 dotenv.config();
 // const __dirname = path.resolve();
-const  port = process.env.PORT|| 5000;
 
 
 
-app.listen(port, ()=>
+server.listen(port, ()=>
 
     {
     connectToMongoDb()
--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -5,6 +5,7 @@ import { useAuthContext } from "../../context/AuthContext";
 import { extractTime } from "../../utils/extractTime";
 import useConversation from "../../zustand/useConversation";
 
+
 const Message = ({ message }) => {
 	const { authUser } = useAuthContext();
 	const { selectedConversation } = useConversation();
@@ -13,6 +14,7 @@ const Message = ({ message }) => {
 	const chatClassName = fromMe ? "chat-end" : "chat-start";
 	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
 	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+  const shakeClass = message.shouldShake ? "shake" : "";
 
 	return (
 		<div className={`chat ${chatClassName}`}>
@@ -26,7 +28,7 @@ const Message = ({ message }) => {
 
 				</div>
 			</div>
-			<div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+					<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
 			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
 		</div>
 	);
